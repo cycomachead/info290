5177f95fd584d2806acd9dcff83974f29adc1915
Michael Ball <cycomachead@gmail.com>
2015-02-21 23:32:59 -0800

    stuffs for python

--- lab5/lab5.py
+++ lab5/lab5.py
@@ -21,19 +21,19 @@ def question2train():
     """ Training the neural net can take a long time, so we save the output for later.
     Also, it may be useful to look into changing the error tolerance. """
     net = nl.net.newff([[np.min(train.TRANSACTION_AMT), np.max(train.TRANSACTION_AMT)]], [10, 1])
-    err = net.train(train.loc[:,["TRANSACTION_AMT"]], train.loc[:,["CAND_ID"]], show = 15)
-    err.save("question2NN.net")
+    err = net.train(train.loc[:,["TRANSACTION_AMT"]], train.loc[:,["CAND_ID"]], show = 1)
+    net.save("question2NN.net")
     return net
 
 def question2load():
     return(nl.load("question2NN.net"))
 
-def question2(retrain = False):
+def question2(retrain = True):
     if retrain:
         return question2train()
     return question2load()
 
-net = question2(retrain = False)
+net = question2(retrain = True)
 
 q2_train_error = np.mean(np.round(net.sim(train.loc[:,["TRANSACTION_AMT"]])[:,0]) == train.CAND_ID)
 q2_test_error = np.mean(np.round(net.sim(test.loc[:,["TRANSACTION_AMT"]])[:,0]) == test.CAND_ID)
