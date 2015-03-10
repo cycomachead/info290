4627fea714cf20c6290e428b7fc8e2859eb92bf8
Michael Ball <cycomachead@gmail.com>
2015-03-10 15:33:21 -0700

    py progress: need to drop from test

--- lab6/michael.py
+++ lab6/michael.py
@@ -6,19 +6,33 @@ train = pd.read_csv("train.csv")
 test = pd.read_csv("test.csv")
 
 rf = ensemble.RandomForestClassifier()
+# Convert Male / Female ==> 0 / 1
+train = train.replace('male', 0)
+train = train.replace('female', 1)
+# Drop NaNs
+train = train[np.isfinite(train['passenger_id'])]
+train = train[np.isfinite(train['pclass'])]
+train = train[np.isfinite(train['sex'])]
+train = train[np.isfinite(train['age'])]
+train = train[np.isfinite(train['sibsp'])]
+train = train[np.isfinite(train['parch'])]
+train = train[np.isfinite(train['fare'])]
+
+
 modified = train.drop("survived", axis = 1)
 modified = modified.drop("name", axis = 1)
 modified = modified.drop("home.dest", axis = 1)
 modified = modified.drop("embarked", axis = 1)
 modified = modified.drop("cabin", axis = 1)
 modified = modified.drop("ticket", axis = 1)
-# Convert Male / Female ==> 0 / 1
-modified = modified.replace('male', 0)
-modified = modified.replace('female', 1)
 
-rf.fit(modified, train[["survived"]])
 
-print(rf)
+
+
+
+thing = rf.fit(modified, train[["survived"]])
+
+print(thing)
 
 
 
