d1470a0c46a8161946405a9e52b5fff642dcb659
Michael Ball <cycomachead@gmail.com>
2015-03-10 15:13:33 -0700

    py progress lab 6

--- lab6/michael.py
+++ lab6/michael.py
@@ -10,6 +10,12 @@ modified = train.drop("survived", axis = 1)
 modified = modified.drop("name", axis = 1)
 modified = modified.drop("home.dest", axis = 1)
 modified = modified.drop("embarked", axis = 1)
+modified = modified.drop("cabin", axis = 1)
+modified = modified.drop("ticket", axis = 1)
+# Convert Male / Female ==> 0 / 1
+modified = modified.replace('male', 0)
+modified = modified.replace('female', 1)
+
 rf.fit(modified, train[["survived"]])
 
 print(rf)
