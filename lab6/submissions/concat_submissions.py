42488a83fcc22edfc939720b84d8220ede4658bc
alecguertin <alecguertin@berkeley.edu>
2015-03-31 22:52:34 -0700

    added bash script to concat files

--- lab6/submissions/concat_submissions.py
+++ lab6/submissions/concat_submissions.py
@@ -11,16 +11,20 @@ for f in files:
         fl = open(f, "r")
         fl = fl.readlines()
         if first:
-            print fl[0]
+            print fl[0].replace("\n", "")
             first = False
 	fl = fl[1:]
 	for l in fl:
             l = l.split(",")
 	    if len(l) > 1:
+                l[0] = int(l[0])
                 l[1] = l[1].replace("\n", "").replace("\"", "")
 		if l[0] not in lines:
                     lines[l[0]] = l[1]
 		else:
                     lines[l[0]] += "," + l[1]
-for i in lines:
-    print "%s,%s"%(i, lines[i])
+
+j = lines.keys()
+j.sort()
+for i in j:
+    print "%d,%s"%(i, lines[i])
