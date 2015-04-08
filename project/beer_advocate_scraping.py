8b82aae2c152580079c906ff480c3a22532d0086
Michael Ball <cycomachead@gmail.com>
2015-04-08 13:40:11 -0700

    add a bunch shit

--- project/beer_advocate_scraping.py
+++ project/beer_advocate_scraping.py
@@ -59,7 +59,8 @@ rdev_matcher = re.compile('^.*rDev\s.*$')
 
 #style_link = style_links[0]
 #style = beer_styles[0]
-for k in range(len(style_links[:])):
+for k in range(10,64): # len(style_links)
+    print('K: ' + str(k))
     style = beer_styles[k]
     style_link = style_links[k]
 
