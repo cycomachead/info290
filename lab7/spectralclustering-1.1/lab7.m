c02348c5148c4700f0ca41e8da449308974a3746
Peter Sujan <peterasujan@gmail.com>
2015-04-09 00:00:54 -0700

    Done with lab7 :).

--- lab7/spectralclustering-1.1/lab7.m
+++ lab7/spectralclustering-1.1/lab7.m
@@ -14,19 +14,68 @@ evals
 %% question 5
 k_heuristic = ceil(log(size(X, 1)));
 for i = -5:5
-    runexpl2(X, 2, k_heuristic + i);
+    runexpl2(X, 2, k_heuristic + i, 0);
     disp(k_heuristic + i);
     pause;
 end
 
 % 7 + 2 = 9 seems to be optimal
 
-%% question 6
-[x1,y1] = randr(50, 5000, 0.10);
+%% question 6 (data2)
+[x1,y1] = randr(50, 5000, 0.20);
 [x2,y2] = randr(20, 4000, 0.05);
 x = [x1 x2]';
 y = [y1 y2]';
 X2 = [x y];
 scatter(x,y);
 
-%% question 7
+%% question 7 data2 clustering
+k_heuristic = ceil(log(size(X2, 1)));
+c2 = runexpl2(X2, 2, k_heuristic + 2, 0);
+
+%% question 7 accuracy
+accuracy([ones(5000, 1); 2 * ones(4000, 1)], c2)
+
+%% question 7 data3
+[x1,y1] = randr(25, 5000, 0.05);
+[x2,y2] = randr(20, 4000, 0.05);
+x = [x1 x2]';
+y = [y1 y2]';
+X3 = [x y];
+scatter(x, y);
+
+%% question 7 data3 clustering
+k_heuristic = ceil(log(size(X3, 1)));
+c3 = runexpl2(X3, 2, k_heuristic + 4, 0);
+
+%% question 7 accuracy
+accuracy([ones(5000, 1); 2 * ones(4000, 1)], c3)
+
+%% question 7 data4
+[x1,y1] = randr(30, 5000, 0.4);
+[x2,y2] = randr(20, 4000, 0.4);
+x = [x1 x2]';
+y = [y1 y2]';
+X4 = [x y];
+scatter(x, y);
+
+%% question 7 data4 clustering
+k_heuristic = ceil(log(size(X4, 1)));
+c4 = runexpl2(X4, 2, k_heuristic + 4, 0);
+
+%% question 7 accuracy
+accuracy([ones(5000, 1); 2 * ones(4000, 1)], c4)
+
+%% question 7 data5
+[x1,y1] = randr(30, 5000, 0.3);
+[x2,y2] = randr(20, 4000, 0.4);
+[x3,y3] = randr(14, 3000, 0.1);
+x = [x1 x2 x3]';
+y = [y1 y2 y3]';
+X5 = [x y];
+scatter(x, y);
+
+%% question 7 data5 clustering
+k_heuristic = ceil(log(size(X4, 1)));
+runexpl2(X5, 3, k_heuristic + 5, 0);
+
--- lab7/spectralclustering-1.1/runexpl2.m
+++ lab7/spectralclustering-1.1/runexpl2.m
@@ -1,10 +1,10 @@
-function cl=runexpl(X2,clusts,nn)
+function cl=runexpl(X2,clusts,nn,sigma)
 %[idx,c]=kmeans(X2,clusts);
 %pause
 %scatter(X2(:,1),X2(:,2),5,idx);
 %scatter(X2(:,1),X2(:,2),5,[ones(400,1);2*ones(400,1)]);
 A=gen_nnd(X2,nn,500);
-[cl ev]=sc(A,0,clusts);
+[cl ev]=sc(A,sigma,clusts);
 ev
 ylabel('X1');xlabel('X2');
 
