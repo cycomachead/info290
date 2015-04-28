b8d98d9c67c45c168c02df17d164b30cb5424541
Michael Ball <cycomachead@gmail.com>
2015-04-28 14:36:27 -0700

    add files to git

---
+++ project/code/analysis/musings.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python3
+
+"""
+To be run from code/analysis
+
+"""
+import sys, os, csv
+
+INPUT = '../../data/'
+# Assume the damn directory exists.
+OUTPUT = '../../processed/'
+
+
+
+if __name__ == '__main__':
+    
