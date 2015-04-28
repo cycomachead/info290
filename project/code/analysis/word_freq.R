fdd8c9a1f3128e878496616348218c34a6d8134f
Peter Sujan <peterasujan@gmail.com>
2015-04-28 14:20:06 -0700

    About to try word freq for all beers.

--- project/code/analysis/word_freq.R
+++ project/code/analysis/word_freq.R
@@ -4,42 +4,54 @@
 common.words <- strsplit(readLines("common-english-words.txt"), ",")[[1]]
 
 #files <- list.files("../../data/American_Pale_Ale_(APA)/", full.names = TRUE)
-files <- list.files("../../data/American_IPA/", full.names = TRUE)
 
-t <- proc.time()
+all.beers <- list.dirs("../../data/")
+all.beers <- all.beers[2:length(all.beers)]
+all.beers.names.only <- sapply(strsplit(all.beers, "//"), function(x) x[2])
+n.beers <- length(all.beers)
 
-n.files <- length(files) - 1
-word.freqs <- vector("list", n.files)
-for (i in 1:n.files) {
-  D <- tryCatch(
-                {
-                  read.csv(files[i], as.is = TRUE, row.names = NULL)
-                }, error = function(e) {
-                  print(paste("error with file:", files[i]))
-                  next
-                })
-  
+for (j in 1:1) {
 
-  text <- D$review_text
+  files <- list.files(all.beers[j], full.names = TRUE)
   
-  text <- gsub("[[:punct:]]", "", text)
+  t <- proc.time()
   
-  text <- tolower(text)
-  
-  text <- strsplit(text, split = "\\s+")
-  
-  text <- Reduce(c, text, c())
-  text <- text[text != ""]
-  
-  counts <- rle(sort(text)) # generates word counts
-  counts <- data.frame(word = counts$value, count = counts$lengths)
-  counts <- counts[!is.element(counts$word, common.words),] # filters out common words
+  n.files <- length(files) - 1
+  word.freqs <- vector("list", n.files)
+  for (i in 1:n.files) {
+    D <- tryCatch(
+                  {
+                    read.csv(files[i], as.is = TRUE, row.names = NULL)
+                  }, error = function(e) {
+                    print(paste("error with file:", files[i]))
+                    next
+                  })
+    
 
-  sorted.indices <- order(counts$count, decreasing = TRUE)
-  counts <- counts[sorted.indices,] # put in decreasing order
-  counts$percent <- counts$count / sum(counts$count)
-#head(counts, 50)
-  word.freqs[[i]] <- counts
+    text <- D$review_text
+    
+    text <- gsub("[[:punct:]]", "", text)
+    
+    text <- tolower(text)
+    
+    text <- strsplit(text, split = "\\s+")
+    
+    text <- Reduce(c, text, c())
+    text <- text[text != ""]
+    
+    counts <- rle(sort(text)) # generates word counts
+    counts <- data.frame(word = counts$value, count = counts$lengths)
+    counts <- counts[!is.element(counts$word, common.words),] # filters out common words
+
+    sorted.indices <- order(counts$count, decreasing = TRUE)
+    counts <- counts[sorted.indices,] # put in decreasing order
+    counts$percent <- counts$count / sum(counts$count)
+                                        #head(counts, 50)
+    word.freqs[[i]] <- counts
+  }
+
+  save(word.freqs, file = paste("word_frequencies_by_beer", all.beers.names.only[j], sep = "/"))
+  
 }
 
 print(proc.time() - t)
