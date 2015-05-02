56288e099e893223e9305644c3f256742947863d
Ubuntu <ubuntu@ip-172-31-24-236.us-west-2.compute.internal>
2015-05-02 06:08:45 +0000

    Built the matrix of counts.

--- project/code/analysis/word_freq_produce_words_list.R
+++ project/code/analysis/word_freq_produce_words_list.R
@@ -1,11 +1,11 @@
 
 all.files <- list.files("word_frequencies_by_beer", full.names = TRUE)
-all.beers.names.only <- sapply(strsplit(all.files, "//"), function(x) x[2])
+all.beers.names.only <- sapply(strsplit(all.files, "/"), function(x) x[2])
 
 all.words <- function(files) {
   words <- NULL
 
-  for (file in all.files) {
+  for (file in files) {
     print(file)
     load(file)
     current.words <- Reduce(function(agg, df) union(agg, df$word[df$count > 1]), word.freqs, NULL) # count > 1 is a simple (though probably overly aggressive) way to remove weird words
@@ -15,10 +15,12 @@ all.words <- function(files) {
 }
 
 words <- all.words(all.files)
-
+save(words, file = "all_words")
 #counts.by.beer <- as.data.frame(setNames(replicate(length(words),numeric(0), simplify = F), words))
 
-counts.by.beer <- matrix(0, nrow = 104 * 50, ncol = length(words))
+library("Matrix")
+
+counts.by.beer <- Matrix(0, nrow = 104 * 50, ncol = length(words))
 beer.names <- NULL
 
 for (i in 1:length(all.files)) {
@@ -28,18 +30,27 @@ for (i in 1:length(all.files)) {
   print(file)
   load(file)
   for (j in 1:length(word.freqs)) {
-    print(j)
-    print(length(word.freqs))
     current <- word.freqs[[j]]
     current.beer <- beers[j]
     shared.words <- intersect(words, current$word)
     columns <- which(is.element(words, shared.words))
     shared.counts <- current[is.element(current$word, shared.words),"count"]
-    counts.by.beer[50 * (i - 1) + j, shared.words] <- shared.counts
+    counts.by.beer[50 * (i - 1) + j, columns] <- shared.counts
     
   }
 }
 
-counts.by.beer <- as.data.frame(counts.by.beer)
-names(counts.by.beer) <- words
+save(counts.by.beer, file = "counts_matrix")
+#counts.by.beer <- as.data.frame(counts.by.beer)
+#names(counts.by.beer) <- words
+#save(counts.by.beer, file = "counts_data_frame")
 
+
+style.labels <- name.labels <- NULL
+for (i in 1:length(all.files)) {
+  beers <- list.files(paste("../../data", all.beers.names.only[i], sep = "/"))
+  style.labels <- c(style.labels, rep(all.beers.names.only[i], length(beers)))
+  name.labels <- c(name.labels, beers[1:length(beers)-1])
+}
+  
+  
