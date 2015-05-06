62124fa827008b96c5f2753285b673c13e906333
Peter Sujan <peterasujan@gmail.com>
2015-05-05 22:30:45 -0700

    Finally finished scaling matrices.

--- project/code/analysis/dim_reduction.R
+++ project/code/analysis/dim_reduction.R
@@ -2,6 +2,8 @@
 library(irlba)
 library(e1071)
 library(randomForest)
+library(parallel)
+library(fpc)
 load("counts_matrix")
 load("labels")
 
@@ -10,24 +12,76 @@ counts.by.beer <- counts.by.beer[name.labels != 0,]
 name.labels <- name.labels[name.labels != 0]
 style.labels <- style.labels[style.labels != 0]
 
-for (i in 1:nrow(counts.by.beer)) {
-    if (i %% 10 == 0) {
-        print(i)
+normalize <- function(x) {
+    s <- sum(x)
+    return(if (s > 0) x / s else x)
+}
+
+normalize.rows <- function() {
+    C <- makeCluster(detectCores()-1)
+    for (i in seq(1, nrow(counts.by.beer)-100, by = 100)) {
+        if (i %% 10 == 1) {
+            print(i)
+        }
+        ## if (any(counts.by.beer[i,] != 0)) {
+        ##     counts.by.beer[i,] <- counts.by.beer[i,] / sum(counts.by.beer[i,])
+        ## }
+        m.temp <- as.matrix(counts.by.beer[i:(i+99),])
+        m.scaled <- matrix(parRapply(C, m.temp, normalize), nrow = 100, byrow = TRUE)
+        counts.by.beer[i:(i+99),] <- m.scaled
     }
-    if (any(counts.by.beer[i,] != 0)) {
-        counts.by.beer[i,] <- counts.by.beer[i,] / mean(counts.by.beer[i,])
+
+    stopCluster(C)
+
+                                        # tail loop
+    for (i in (i+1):nrow(counts.by.beer)) {
+        if (any(counts.by.beer[i,] != 0)) {
+            counts.by.beer[i,] <- counts.by.beer[i,] / sum(counts.by.beer[i,])
+        }
     }
 }
 
-save(counts.by.beer, file = "counts_matrix_scaled")
+normalize.rows()
+gc()
+save(counts.by.beer, file = "counts_matrix_normalized_rows")
 
-## decomp <- irlba(counts.by.beer, nu = 0, nv = 50)
+normalize.cols <- function() {
+    C <- makeCluster(detectCores()-1)
+    for (i in seq(1, ncol(counts.by.beer)-500, by = 500)) {
+        if (i %% 10 == 1) {
+            print(i)
+        }
+        ## if (any(counts.by.beer[i,] != 0)) {
+        ##     counts.by.beer[i,] <- counts.by.beer[i,] / sum(counts.by.beer[i,])
+        ## }
+        m.temp <- as.matrix(counts.by.beer[,i:(i+499)])
+        m.scaled <- matrix(parCapply(C, m.temp, normalize), ncol = 500, byrow = FALSE)
+        counts.by.beer[,i:(i+499)] <- m.scaled
+    }
 
-## pcs <- counts.by.beer %*% decomp$v
+    stopCluster(C)
 
-## #svm.50 <- svm(as.data.frame(as.matrix(pcs)), style.labels, scale = F)
+    # tail loop
+    for (i in (i+1):ncol(counts.by.beer)) {
+        if (any(counts.by.beer[,i] != 0)) {
+            counts.by.beer[,i] <- counts.by.beer[,i] / sum(counts.by.beer[,i])
+        }
+    }
+}
 
-## pcs.df <- as.data.frame(as.matrix(pcs))
+normalize.cols()
+gc()
+save(counts.by.beer, file = "counts_matrix_normalized_rows_cols")
+
+load("counts_matrix_normalized_rows_cols")
+
+decomp <- irlba(counts.by.beer, nu = 0, nv = 50)
+
+pcs <- counts.by.beer %*% decomp$v
+
+pcs.df <- as.data.frame(as.matrix(pcs))
+
+#svm.50 <- svm(pcs.df, style.labels, scale = F)
 
 ## rf <- randomForest(pcs.df, as.factor(style.labels), scale = F)
 
@@ -35,4 +89,6 @@ save(counts.by.beer, file = "counts_matrix_scaled")
 
 ## # clustering
 
-## clust <- kmeans(pcs.df, centers = 104)
+clust <- kmeans(pcs.df, centers = 104, iter.max = 30)
+
+clust.stats <- cluster.stats(clustering = clust$cluster, alt.clustering = as.integer(as.factor(style.labels)), compareonly = TRUE)
